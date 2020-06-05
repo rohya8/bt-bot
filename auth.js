33bab91663470bc07187c53dea58fe0ebbcbbcaa
Rohit Wadke <rohit.m.wadke@gmail.com>
2020-06-05 20:11:55 +0530

    Api Chnages

--- auth.js
+++ auth.js
@@ -1,7 +1,6 @@
 module.exports = {
-    consumer_key: '',
-    consumer_secret: '',
-    access_token: '',
-    access_token_secret: '',
-    timeout_ms: 60*1000
+    consumer_key: '3mkB0HtpDZVSAaEnuy7bOSG6b',
+    consumer_secret: 'zJUW4tRp26RummoCFdChUyPOcOEPDFWxRH5QFnQp0MyrgILcZ7',
+    access_token: '1268254122655879168-pt6PUl6LvfGYkibV2H0UBTK8xSulOd',
+    access_token_secret: 'YnoJYjVLahtdBVNLS9QR8su1iOwPK9qYCvfcWVWfeGzyS'
  }
---
+++ bot.js
@@ -0,0 +1,54 @@
+var config = require('./auth');
+const axios = require('axios');
+const twit = require('twit');
+var T = new twit(config);
+
+var url = 'https://icanhazdadjoke.com/';
+
+retrieveJoke();
+
+//Posting the tweet!
+function tweetPost(msg) {
+    var tweet = {
+        status: msg
+    }
+    T.post('statuses/update', tweet, function (err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(data.id);
+        }
+    });
+}
+
+function retrieveJoke() {
+    try {
+        getJoke();
+        setTimeout(retrieveJoke, 3600000);
+        // setTimeout(retrieveJoke, 1000 * 60 * 10);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+function getJoke() {
+    axios.get(url, {
+        headers: {
+            Accept: 'application/json'
+        }
+    }).then(function (response) {
+            // success
+            if (response && response.data && response.data.joke && response.data.joke.length < 242) {
+                console.log('In if' + response.data.joke.length);
+                response.data.joke += ' #joke #lmao #memesdaily #braintickler';
+                tweetPost(response.data.joke);
+            } else {
+                console.log('In else' + response.data.joke.length);
+              //  getJoke();
+            }
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        });
+}
