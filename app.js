9b2ad97249239a8765f4ea2ba950d1dadb5e587c
Rohit Wadke <rohit.m.wadke@gmail.com>
2020-06-13 23:18:30 +0530

    Port Change

--- app.js
+++ app.js
@@ -5,6 +5,4 @@ const app = express();
 const {tweetController} = require('./router/tweet.router'); 
 
 app.use('/helloWorld',tweetController);
-app.listen(3000,() => {
-    console.log("Started on PORT 3000");
-  })
+app.listen(process.env.PORT, '0.0.0.0' | 3000,() => console.log('Started server at 3000'));
